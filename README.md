bcb50e22c0b2dcea7b8864be55827d781d3e90c2
Mark Coccimiglio <mc76@rice.edu>
2017-11-12 20:22:38 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+
+pam_usbkey Copyright (c) 2017 Mark Coccimiglio
+mcoccimiglio@rice.edu
+
+The design of the pam module is to utilize SSH Keys for local login
+authentication.  The basic premise is to install pass-phrase protected
+private SSH keys on a USB fob to authenticate with a computer.  If the
+corresponding Public SSH Key is present for the requested user ID then
+access is granted.
+
+Required packages:
+BASH, PERL, AT, OpenSSL(-devel), SED, libssh2(-devel)
+
+The following scripts are included in this package:
+
+makeusbkey - make a USB Key.
+keytemp - Places an SSH Key into a users account (for diagnostics).
---
+++ pam_usbkey.c
@@ -0,0 +1,76 @@
+/*
+    pam_usbkey -- PAM Module to use SSH Keys for LOCAL authentication.
+    Copyright (c) 2017 Mark Coccimiglio
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+*/
+
+#define __VERSION__  "0.0.1"
+#define __AUTHOR__ "Mark Coccimiglio"
+
+#define PAM_SM_AUTH
+#include <security/pam_modules.h>
+#include <security/_pam_macros.h>
+#include <libssh2.h>
+#include <syslog.h>
+
+#define __APP__ pam_usbkey
+#define USBKEY_CONF /etc/usbkey.conf
+
+void l_error (int error);
+
+
+PAM_EXTERN int
+ pam_sm_authenticate
+  (pam_handle_t *pamh, int flags, int argc, const char **argv)
+   {
+
+        const char      *service;
+        const char      *user;
+        const char      *tty;
+        int             rval;
+
+        rval = pam_get_item(pamh, PAM_SERVICE, (const void **)(const void *)&service);
+
+        if (rval != PAM_SUCCESS)
+        {
+                l_error("Unable to retrieve the PAM service name.\n");
+                return (PAM_AUTH_ERR);
+        }
+
+        /* Find, load and "try" to decrypt private key(s) using provided password */
+
+        /* ssh-keygen -y -f mykey.pem > mykey.pub */
+        /* grep "$(ssh-keygen -P PassPhrase -y -f id_rsa.test 2>&1 )" ~/.ssh/authorized_keys */
+
+        /* Compare Private key(s) against user's public key(s) in ~/.ssh/authorized_keys */
+
+  }
+
+PAM_EXTERN int
+  pam_sm_setcred
+   (pam_handle_t *pamh,int flags,int argc, const char **argv)
+    {
+        return (PAM_SUCCESS);
+    }
+
+void l_error (char* error)
+ {
+      openlog( "__APP__", LOG_PID, LOG_AUTHPRIV);
+      syslog(LOG_ERR, error);
+      closelog();
+ }
