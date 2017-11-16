598b065c8cde6092e461c183a6c3f9b9052420e4
Mark Coccimiglio <mc76@rice.edu>
2017-11-16 17:05:49 -0600

    test4

--- test4.c
+++ test4.c
@@ -6,6 +6,18 @@
 
 main () {
   int rval;
-  rval = system ("grep \"$(ssh-keygen -P PassPhrase -y -f %s 2>&1 )\" /root/.ssh/authorized_keys");
-  printf("rval= %d \n", rval);
+  FILE *cmdFP;
+  char buff[2048];
+
+  /* rval = system ("grep \"$(ssh-keygen -P PassPhrase -y -f %s 2>&1 )\" /root/.ssh/authorized_keys");
+  printf("rval= %d \n", rval); */
+  cmdFP = popen("grep \"$(ssh-keygen -P PassPhrase -y -f /dev/vdb1 2>&1 )\" /root/.ssh/authorized_keys | cut -d' ' -f3", "r");
+  if (fp == NULL) {
+    printf("Failed to run command\n" );
+    exit(1);
+  }
+   while (fgets(path, sizeof(path)-1, cmdFP) != NULL) {
+    printf("%s", path);
+  }
+  pclose (cmdFP);
 }
