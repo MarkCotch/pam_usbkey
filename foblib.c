83c2ffe4fc1078c3b39808e09a62c8ceb3855e97
Mark Coccimiglio <mc76@rice.edu>
2017-11-17 06:43:37 -0600

    Stupid syntax error.

--- foblib.c
+++ foblib.c
@@ -42,7 +42,7 @@ void l_record (char* _message) {
   closelog(); */
   char _tmpString[256]={0};
   time_t _now=time(NULL);
-  FILE _FH fopen=("/var/log/pam_sshkey.log", "s");
+  FILE *_FH=fopen("/var/log/pam_sshkey.log", "s");
   fprint (_FH,"pam_usbkey: %s : %s\n", ctime, _message);
   fclose (_FH);
 }
