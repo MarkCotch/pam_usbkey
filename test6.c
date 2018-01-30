6392bcc521abab65d6815f88cd45a51a159f5e7e
Mark Coccimiglio <mc76@rice.edu>
2018-01-29 21:06:14 -0600

    Purge unnecessary test files.

--- test6.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-main () {
-	printf("at line %d\n", __LINE__);	
-}
--- test7.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-main (){
-	int loop;
-	for (loop=0 ; loop < 255 ; loop++) {
-		printf ("%d : %c \n", loop, loop);
-	}
-}
