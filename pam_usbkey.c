c4988d8072e15548d46aeed559fc461ac002e33b
Mark Coccimiglio <mc76@rice.edu>
2018-01-30 07:59:17 -0600

    Clean up debugging annotation.

--- pam_usbkey.c
+++ pam_usbkey.c
@@ -21,7 +21,9 @@
 
 #define __PUK_VERSION__  "0.0.2"
 #define __AUTHOR__ "Mark Coccimiglio"
-#define __MYDEBUG__ (0)
+#ifndef __DEBUG__
+  #define __DEBUG__ (0)
+#endif
 
 #define PAM_SM_AUTH
 #include <security/pam_modules.h>
@@ -57,19 +59,19 @@ PAM_EXTERN int
         int             rval;
         char _tempString[256]={0};
 
-        if (__MYDEBUG__) l_record("DEBUG:pam_usbkey called. ");
+        if (__DEBUG__) l_record("DEBUG:pam_usbkey called. ");
         if ( pam_get_item(pamh, PAM_SERVICE, (const void **)(const void *)&service ) != PAM_SUCCESS || !service || !*service) {
           l_record ("Unable to retrieve the PAM service name for :%s", service);
           return (PAM_AUTH_ERR);
         }
-        if (__MYDEBUG__) l_record("DEBUG:We have service %s ", service);
+        if (__DEBUG__) l_record("DEBUG:We have service %s ", service);
         if (pam_get_item( pamh, PAM_USER, (const void **)(const void *)&user ) != PAM_SUCCESS || !user || !*user) {
           l_record ("Unable to retrieve the PAM user name for :%s", user);
           return (PAM_AUTH_ERR);
         }
-        if (__MYDEBUG__) l_record("DEBUG:We have user: %s ", user);
+        if (__DEBUG__) l_record("DEBUG:We have user: %s ", user);
         pam_get_item( pamh, PAM_AUTHTOK, (const void **)(const void *)&pre_token );
-        if (__MYDEBUG__) l_record("DEBUG:We have pre_token: %s", pre_token);
+        if (__DEBUG__) l_record("DEBUG:We have pre_token: %s", pre_token);
 
         /* First test that the user is recognized by the system and has a home directory. (Sanity Checking) */
         struct passwd *_userInfo=getpwnam(user);
@@ -77,7 +79,7 @@ PAM_EXTERN int
           l_record ("Unable to locate user ID : %s ", user);
           return (PAM_AUTHINFO_UNAVAIL);
         }
-        if (__MYDEBUG__) l_record("DEBUG:We have validated user: %s ", user);
+        if (__DEBUG__) l_record("DEBUG:We have validated user: %s ", user);
 
         DIR *_homeDIR;
         if (! (_homeDIR=opendir(_userInfo->pw_dir) ) ) {
@@ -86,32 +88,28 @@ PAM_EXTERN int
           return (PAM_AUTHINFO_UNAVAIL);
         }
         closedir (_homeDIR);
-        if (__MYDEBUG__) l_record("DEBUG:we have validated home dir: %s", _userInfo->pw_dir);
+        if (__DEBUG__) l_record("DEBUG:we have validated home dir: %s", _userInfo->pw_dir);
 
         /* Sanitize input from user.  Cannot accept passwords that contain ', ", *, \ or $  */
-/*        if ( testForBadChar(token) ) {
-          l_record ("Bad Character(s) in token: %s", token );
-          return (PAM_AUTH_ERR);
-        } */
         strcpy(token, pre_token);
         sanitizeString(token);
-        if (__MYDEBUG__) l_record("DEBUG:we have sanitized token: %s", token);
+        if (__DEBUG__) l_record("DEBUG:we have sanitized token: %s", token);
 
         /* Find, load and "try" to decrypt private key(s) using provided password */
 
         if (! findKeyFOB(keyFOB) ) {
           /* This represents a failure to to find an authentication
               FOB.  At this point we should fail out silently unless DEBUG.*/
-          if (__MYDEBUG__) l_record("DEBUG:No Key FOB found. Returning PAM_AUTHINFO_UNAVAIL");
+          if (__DEBUG__) l_record("DEBUG:No Key FOB found. Returning PAM_AUTHINFO_UNAVAIL");
           return (PAM_AUTHINFO_UNAVAIL);
         }
-        if (__MYDEBUG__) sleep (5);
+        if (__DEBUG__) sleep (5);
         l_record ("Found Authentication FOB %s ", keyFOB );
 
         /* Check FOB device permissions.  og-rwx is a necessity.*/
         /* for now just do it.  We can clean this up later. */
         sprintf (_tempString, "chmod 600 %s", keyFOB);
-        if (__MYDEBUG__) l_record ("DEBUG:set FOB to correct perms: %s", _tempString );
+        if (__DEBUG__) l_record ("DEBUG:set FOB to correct perms: %s", _tempString );
         system (_tempString);
 
         char keyLabel[512]={0};
@@ -123,7 +121,7 @@ PAM_EXTERN int
            "grep \"$(ssh-keygen -P \"%s\" -y -f %s 2>&1 )\" %s/.ssh/authorized_keys /root/.ssh/authorized_keys 2> /dev/null | head -1"
            ,token, keyFOB, _userInfo->pw_dir); */
         sprintf(cmdString, "ssh-keygen -P \"%s\" -y -f %s 2>&1", token, keyFOB );
-        if (__MYDEBUG__) l_record ("DEBUG:CMD String: %s ", cmdString);
+        if (__DEBUG__) l_record ("DEBUG:CMD String: %s ", cmdString);
         _ssh_keygenFP = popen(cmdString, "r");
         sleep(1);
         if (_ssh_keygenFP == NULL) {
@@ -138,7 +136,7 @@ PAM_EXTERN int
           l_record("Derived pubkey from private returned no data.");
           return(PAM_AUTHINFO_UNAVAIL);
         }
-        if (__MYDEBUG__) l_record ("DEBUG:We have keyLabel : '%s' ", keyLabel);
+        if (__DEBUG__) l_record ("DEBUG:We have keyLabel : '%s' ", keyLabel);
 
         if ( _stringCompare( "load failed", keyLabel, 11 ) ) {
           l_record("Bad password for user %s", user);
@@ -150,7 +148,7 @@ PAM_EXTERN int
         /* extract the actual private key*/
         privKey=strtok(keyLabel, " \n");
         privKey=strtok(NULL , " \n");
-        if (__MYDEBUG__) l_record ("DEBUG:privKey is: '%s' ", privKey);
+        if (__DEBUG__) l_record ("DEBUG:privKey is: '%s' ", privKey);
 
         /* Next we need to roll through ~/.ssh/authorized_keys and/or
            /root/.ssh/authorized_keys to see if our keys match. */
@@ -166,7 +164,7 @@ PAM_EXTERN int
         }
 
         while ( fgets ( pubKeyToTest, 512, authFP) !=NULL ) {
-          if (__MYDEBUG__) l_record ("DEBUG:Trying Key: '%s' ", pubKeyToTest);
+          if (__DEBUG__) l_record ("DEBUG:Trying Key: '%s' ", pubKeyToTest);
           if ( ! strlen(pubKeyToTest) ) {
             l_record ("pubKeyToTest length NULL...continue.");
             continue;
@@ -187,7 +185,7 @@ PAM_EXTERN int
             time_t _now=time(NULL);
             sprintf (_tmpfile, "/tmp/.pam_usbkey-%d-%d", _now, rval);
 
-            if (__MYDEBUG__) l_record ("DEBUG:Creating temp file '%s'", _tmpfile);
+            if (__DEBUG__) l_record ("DEBUG:Creating temp file '%s'", _tmpfile);
             if ( (_tempPubKeyFH=fopen(_tmpfile, "w")) == NULL ) {
               l_record ("Unable to open file '%s' for write", _tempPubKeyFH);
               fclose (_tempPubKeyFH);
@@ -202,12 +200,12 @@ PAM_EXTERN int
             char getSigCmd[1024]={0};
             /* strtok(pubKeyToTest, "\n"); */
             sprintf(getSigCmd, "/usr/bin/ssh-keygen -lf %s " , _tmpfile);
-            if (__MYDEBUG__) l_record ("DEBUG:Running: '%s' ", getSigCmd);
+            if (__DEBUG__) l_record ("DEBUG:Running: '%s' ", getSigCmd);
 
             _tGetSigFH=popen(getSigCmd, "r");
-            /* if (__MYDEBUG__) sleep (1); */
+            /* if (__DEBUG__) sleep (1); */
             if ( _tGetSigFH == NULL ) { l_record ("Failed to obtain key Fingreprint."); pclose (_tGetSigFH); return (PAM_AUTHINFO_UNAVAIL); }
-            if (__MYDEBUG__) { if (_tGetSigFH) l_record ("DEBUG:Command run successfull."); }
+            if (__DEBUG__) { if (_tGetSigFH) l_record ("DEBUG:Command run successfull."); }
 
             fgets( _sVal, 512, _tGetSigFH );
             strtok(_sVal, "\n");
@@ -216,7 +214,7 @@ PAM_EXTERN int
             l_record ("Key authorized. Fingerprint: '%s' ", _sVal );
             pclose (_tGetSigFH);
 
-            if (__MYDEBUG__) l_record ("DEBUG: Removing temp file %s", _tmpfile);
+            if (__DEBUG__) l_record ("DEBUG: Removing temp file %s", _tmpfile);
             if (remove (_tmpfile)) l_record ("Removing temp file %s FAILED.", _tmpfile);
 
             return (PAM_SUCCESS);
@@ -234,6 +232,6 @@ PAM_EXTERN int
   pam_sm_setcred
    (pam_handle_t *pamh,int flags,int argc, const char **argv)
     {
-        if (__MYDEBUG__) l_record("DEBUG:pam_usbkey::pam_sm_setcred Does nothing.  Returning PAM_SUCCESS");
+        if (__DEBUG__) l_record("DEBUG:pam_usbkey::pam_sm_setcred Does nothing.  Returning PAM_SUCCESS");
         return (PAM_SUCCESS);
     }
