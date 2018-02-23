97d58b8c0c9c7b7088692b17e370b826804b33a5
Mark Coccimiglio <mc76@rice.edu>
2018-02-22 21:38:30 -0600

    clarified some logging.

--- pam_usbkey.c
+++ pam_usbkey.c
@@ -195,17 +195,17 @@ PAM_EXTERN int
         char userAuthorized_keys[256];
         sprintf (userAuthorized_keys, "%s/.ssh/authorized_keys", _userInfo->pw_dir);
         if (testResults=testKeys(userAuthorized_keys, privKey)) {
-          l_record ("Matching key found in '%s' ", userAuthorized_keys);
+          l_record ("pam_usbkey: Matching key found in '%s' ", userAuthorized_keys);
           l_record ("pam_usbkey: success for user: '%s' ", user);
-          l_record ("Key authorized. Fingerprint: '%s' ", testResults );
+          l_record ("pam_usbkey: Key authorized. Fingerprint: '%s' ", testResults );
           free (testResults);
           return (PAM_SUCCESS);
         }
         /* Test /root/.ssh/authorized_keys to see if our keys match. */
         if (testResults=testKeys("/root/.ssh/authorized_keys", privKey ) ) {
-          l_record ("Matching key found in '%s' ", "/root/.ssh/authorized_keys");
+          l_record ("pam_usbkey: Matching key found in '%s' ", "/root/.ssh/authorized_keys");
           l_record ("pam_usbkey: success for user: '%s' ", user);
-          l_record ("Key authorized. Fingerprint: '%s' ", testResults );
+          l_record ("pam_usbkey: Key authorized. Fingerprint: '%s' ", testResults );
           free (testResults);
           return (PAM_SUCCESS);
         }
