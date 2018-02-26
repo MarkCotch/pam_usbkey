5140374ab47af1c9768efb3ddafbd1d4263398a8
Mark Coccimiglio <mc76@rice.edu>
2018-02-26 15:35:53 -0600

    added handlers for pam_sm_acct_mgmt, pam_sm_open_session, pam_sm_close_session and pam_sm_chauthtok .

--- pam_usbkey.c
+++ pam_usbkey.c
@@ -244,3 +244,30 @@ PAM_EXTERN int
       return (PAM_SUCCESS);
 
     }
+
+
+PAM_EXTERN int
+    pam_sm_acct_mgmt
+    (pam_handle_t *pamh, int flags, int argc, const char *argv[]) {
+    	return (PAM_SUCCESS);
+}
+
+PAM_EXTERN int
+  pam_sm_open_session
+     (pam_handle_t *pamh, int flags, int argc, const char *argv[]) {
+    	  return (PAM_SUCCESS);
+}
+
+PAM_EXTERN int
+  pam_sm_close_session
+    (pam_handle_t *pamh, int flags, int argc, const char *argv[]) {
+    	return (PAM_SUCCESS);
+}
+
+PAM_EXTERN int
+  pam_sm_chauthtok
+    (pam_handle_t *pamh, int flags, int argc, const char *argv[]) {
+      return (PAM_SERVICE_ERR);
+}
+
+    
